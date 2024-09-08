b63d277bda1f19e585b1f3d2d57c8553881128be
pear <pearguacamole40zr@gmail.com>
2024-09-08 15:35:02 +0530

    Initial Commit

---
+++ GO_BASICS/values.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("!+!=", 1+1)
+	fmt.Println("9.0/4.0=", 9.0/4.0)
+	fmt.Println("9.0/4.0=", 9.0/4)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
